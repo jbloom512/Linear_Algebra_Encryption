e5140c030067ec20d691f9207ad257159b1b1270
Joey Bloom <joeybloom@Joeys-MacBook-Air.local>
2017-09-19 12:12:33 -0500

    Wrote function to convert our text message into matrix-vector form

--- Conversion.py
+++ Conversion.py
@@ -9,6 +9,18 @@ def sanitize(text):
 
 
 def generate_cipher_matrix(text, size):
+    text = sanitize(text)
+    matrix = []
+    count = 0
+    for i in range(size):
+        row = []
+
+        for num in range(size):
+            row.append(text[count])
+            count += 1
+        matrix.append(row)
+        print(matrix)
+    print(matrix)
     return matrix
 
 
@@ -16,5 +28,6 @@ def convert(key, text):
     return matrix
 
 text="Hello!?"
+generate_cipher_matrix(text,3)
 
-print(sanitize(text))
+#print(sanitize(text))
