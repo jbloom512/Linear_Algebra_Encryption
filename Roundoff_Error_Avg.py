19a771e768ca197e61a42cd708334ea8834b057f
Joey Bloom <joeybloom@Joeys-MacBook-Air.local>
2017-09-24 12:44:07 -0500

    Changed decryption key function. Average errors are now 0 :)

--- Roundoff_Error_Avg.py
+++ Roundoff_Error_Avg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+  #!/usr/bin/env python3
 import numpy as np
 import Conversion
 import Generate_Encryption_Key
@@ -14,22 +14,22 @@ def hill_cipher_algorithm(message_to_encrypt,key_size):
         message_matrix = Conversion.generate_cipher_matrix(message_to_encrypt,key_size)
         encryption_key = Generate_Encryption_Key.generate_encryption_key(key_size)
         encrypted_message = Conversion.encrypt_message(encryption_key,message_matrix)
-
-        decryption_key = Generate_Encryption_Key.generate_decryption_key(encryption_key)
+        decryption_key = Generate_Encryption_Key.generate_sympy_decryption_key(encryption_key)
         decrypted_message = Conversion.decrypt_message(decryption_key,encrypted_message)
         #print(message_matrix)
         #print(encrypted_message)
         #print(decrypted_message)
-        count += 1
         if np.array_equiv(message_matrix,decrypted_message) == True:
-            #print('Success!')
+            #print('Success')
             success = True
         else:
             pass
+        count += 1
+
     return count
 
 message_to_encrypt = 'This is a random test!? to see how we do, i hope well.!'
-key_size = 11
+key_size = 5
 
 # Key Size of 11 yields average of 4-5 attempts
 # Anything above 11, the algorithm starts to freeze up and encounted more round off errors
